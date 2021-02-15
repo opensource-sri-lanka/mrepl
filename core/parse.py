19819aa32701e3dc734ddc59b873aa57008abb02
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-15 16:10:42 +0530

    basic commands

---
+++ core/parse.py
@@ -0,0 +1,18 @@
+def add(x, y):
+    output = x + y
+    return output
+
+
+def substract(x, y):
+    output = x - y
+    return output
+
+
+def multiply(x, y):
+    output = x * y
+    return output
+
+
+def divide(x, y):
+    output = x / y
+    return output
