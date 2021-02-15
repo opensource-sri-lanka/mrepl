6e6c00535536214738c2d5f93c6d997e9044e78d
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-15 16:16:30 +0530

    welcome

--- application.py
+++ application.py
@@ -1,8 +1,18 @@
+import core.parse
+
+
+def welcome():
+    print("Welcome to MREPL")
+    print("Use this at your own risk")
+    print("Authors: Tarith Jayasooriya, Dinuda Yaggahavita")
+    prompt()
+
 
 def prompt():
     while True:
         inp = input()
-        answer = "no answer" # this variable will hold the answer of the input
+        answer = "no answer"  # this variable will hold the answer of the input
         print(answer)
 
-prompt()
+
+welcome()
