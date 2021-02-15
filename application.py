d682d4c11643628e8905772ebf19916922939d21
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-15 18:42:38 +0530

    add presence

--- application.py
+++ application.py
@@ -10,15 +10,21 @@ def welcome():
     print("Welcome to MREPL")
     print("Use this at your own risk")
     print("Authors: Tarith Jayasooriya, Dinuda Yaggahavita")
+    print("")  # leave a space
 
 
 def prompt():
     print("Enter your equation: ")
     # Hold the input
     inp = input()
+    # Checks if the input is valid
     if (len(inp) > 0):
         # find the operators
-        print("input detected")
+        add = inp.find("+")
+        if(add > 0):
+            print("Has addition")
+        else:
+            print("No addition")
     else:
         print("Please enter a valid equation")
         prompt()
