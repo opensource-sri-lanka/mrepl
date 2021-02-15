f3df1114d7abaa011fa53331ce3b758c0ebc8612
Tarith <tarithj@gmail.com>
2021-02-15 16:45:59 +0530

    created parse.add function

--- core/parse.py
+++ core/parse.py
@@ -1,6 +1,13 @@
-def add(x, y):
-    output = x + y
-    return output
+def add(mathDef):
+    out = 0
+    b = mathDef.split("+")
+
+    # parsing
+    i = 0
+    while i < len(b):
+        out += int(b[i])
+        i += 1
+    return out
 
 
 def substract(x, y):
